2f2523aee95eb6c94eb49dfa482bb8f79d8eafa5
karl-kaarel <karlmathias.fenske@gmail.com>
2023-01-10 15:49:31 +0000

    Unit Tests for CRUD

--- src/SpotifyCloneApp/tests.py
+++ src/SpotifyCloneApp/tests.py
@@ -1,3 +1,67 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from SpotifyCloneApp.models import Playlists
+from SpotifyCloneApp.views import SpotifyApi
+import json
+
+# class ApiUrlsTests(SimpleTestCase):
+#     def tests_GET(self):
+#         url = reverse('playlists')
+#         print(url)
 
 # Create your tests here.
+
+class TestViews(TestCase):
+    def test_GET(self):
+        c = Client()
+        #ToBeSent
+        response = c.get(reverse("playlists"))
+
+        #ToReceive
+        self.assertEquals(response.status_code, 200)
+
+    def test_POST_PUT_DELETE(self):
+        c = Client()
+        data_post = {
+            "name":"Tango"
+        }
+        data_put = {
+            "id": 1,
+            "name":"Funk"
+        }
+
+        data_delete = {
+            "id": 1,
+        }
+
+        #ToBeSent
+        response_post = c.post(reverse("playlists"), data_post, content_type="application/json")
+        response_get_post = c.get(reverse("playlists"))
+
+        response_put = c.put(reverse("playlists"), data_put, content_type="application/json")
+        response_get_put = c.get(reverse("playlists"))
+
+        response_delete = c.delete(reverse("playlists", args=[1]))
+        response_get_delete = c.get(reverse("playlists"))
+
+        #ToReceive
+        self.assertEquals(response_post.status_code, 200)
+        self.assertEquals(response_post.json(), "Added successfuly")
+
+        self.assertEquals(response_get_post.status_code, 200)
+        self.assertEquals(response_get_post.json(), {'items': [{'id': 1, 'name': 'Tango'}]})
+        print(response_get_post.json())#Tango after post
+
+        self.assertEquals(response_put.status_code, 200)
+        self.assertEquals(response_put.json(), "Update successful")
+
+        self.assertEquals(response_get_put.status_code, 200)
+        self.assertEquals(response_get_put.json(), {'items': [{'id': 1, 'name': 'Funk'}]})
+        print(response_get_put.json())#Funk after update
+
+        self.assertEquals(response_delete.status_code, 200)
+        self.assertEquals(response_delete.json(), "Deleted successfuly")
+
+        self.assertEquals(response_get_delete.status_code, 200)
+        self.assertEquals(response_get_delete.json(), {'items': []})
+        print(response_get_delete.json())#empty after delete
--- src/SpotifyCloneApp/urls.py
+++ src/SpotifyCloneApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from SpotifyCloneApp import views
 
 urlpatterns=[
-    re_path(r'^playlists/$', views.SpotifyApi),
-    re_path(r'^playlists/([0-9]+)$',views.SpotifyApi)
+    re_path(r'^playlists/$', views.SpotifyApi, name="playlists"),
+    re_path(r'^playlists/([0-9]+)$',views.SpotifyApi, name="playlists")
 ]
